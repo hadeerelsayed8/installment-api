83ad9f7d73b83439628f2f96cff06f79bbee029c
hadeer <info@nile-soft.com>
2026-05-29 09:50:16 +0300

    fix port

--- server.js
+++ server.js
@@ -21,6 +21,8 @@ app.get('/', (req, res) => {
   res.send('API is running 🚀');
 });
 
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
