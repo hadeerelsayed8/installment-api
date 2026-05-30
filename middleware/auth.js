26ef0dbef12c964a31bbcde955761430f454a2da
hadeer <info@nile-soft.com>
2026-05-31 00:41:01 +0300

    add middleware auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,48 @@
 const jwt = require('jsonwebtoken');
 
+const SECRET = process.env.JWT_SECRET;
+
+module.exports = (req, res, next) => {
+
+  try {
+
+    const authHeader =
+      req.headers['authorization'];
+
+    if (!authHeader) {
+
+      return res.status(401).json({
+        error: 'No token provided'
+      });
+
+    }
+
+    const token =
+      authHeader.split(' ')[1];
+
+    const decoded =
+      jwt.verify(token, SECRET);
+
+    req.user = decoded;
+
+    next();
+
+  }
+
+  catch (err) {
+
+    return res.status(401).json({
+      error: 'Invalid token'
+    });
+
+  }
+
+};
+
+
+/*
+const jwt = require('jsonwebtoken');
+
 const SECRET = "mysecretkey";
 
 module.exports = (req, res, next) => {
@@ -30,4 +73,4 @@ module.exports = (req, res, next) => {
       error: 'Invalid token'
     });
   }
-};
+};*/
